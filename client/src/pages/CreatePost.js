0117124d2bad6abeced969889f58e2af9eaa0d4a
Fauzia Shafi <fauziashafi12@gmail.com>
2022-03-12 19:13:01 -0800

    Added Static PostList and CreatePost page

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,13 +1,86 @@
 export default function Create() {
-    
-    return (
-      <div>
-        {/* TODO: display logged in user's username */}
-        <h1>Make a Post</h1>
-        <hr />
-        <p>
-          ...
-        </p>
+  const styles = {
+    body: {
+      backgroundColor: "grey",
+     
+      fontFamily: "Anton",
+    },
+    form: {
+      padding: "8%",
+      margin: "2em",
+      backgroundColor: "light-grey",
+      width: "50%",
+      height: "70%",
+      borderRadius: "7px",
+      
+      
+    },
+
+    center: {
+     display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "80vh",
+      flexDirection: "column",
+    },
+    formControl: {
+      display: "flex",
+      padding: "0.50em",
+    },
+    label: {
+      flex: "0 1 6em",
+      paddingRight: "0.25em",
+    },
+    input: {
+      flex: "0 1 20em",
+      paddingRight: "0.25em",
+    },
+    btn: {
+      borderRadius: "7px",
+      marginTop: "2em",
+      marginRight: "1em",
+    },
+  };
+
+  return (
+    <div style={styles.body}>
+      <div style={styles.center}>
+        
+        <form style={styles.form}>
+          <div style={styles.formControl}>
+            <label htmlFor="text" style={styles.label}>
+              Title
+            </label>
+            <input
+              id="title"
+              type="text"
+              name="title"
+              placeholder="Your title"
+              style={styles.input}
+            />
+          </div>
+          <div style={styles.formControl}>
+            <label htmlFor="text" style={styles.label}>
+              Your post
+            </label>
+            <textarea
+              id="post"
+              type="text"
+              name="post"
+              placeholder="Your thoughts..."
+              style={styles.input}
+            />
+          </div>
+          <div style={styles.formControl}>
+            <button type="submit" style={styles.btn}>
+              Post
+            </button>
+            <button type="submit" style={styles.btn}>
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
