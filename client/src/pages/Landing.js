dd2de07c5f74b1bb3492c4467b01dacc074b4757
maxman3789 <maxman3789@aol.com>
2022-03-13 16:48:33 -0700

    navbar landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
 import "../styles/landing.css";
+import { NavLink } from "react-router-dom";
+
 
 export default function Landing() {
 
@@ -11,8 +13,14 @@ export default function Landing() {
             <p className="logo">Welcome to Happy Programmers</p>
             <p>Sign up to collobarate and be happy with fellow Programmers working towards Solutions.</p>
         </div>
-        <Button className="btn">Login</Button>
-        <Button className="btn">Signup</Button>
+        {/* <Button className="btn" to="/login">Login</Button>
+        <Button className="btn" to="/signup">Signup</Button> */}
+        <NavLink to="/login" className="btn">
+            Login
+          </NavLink>
+          <NavLink to="/signup" className="btn">
+            Signup
+          </NavLink>
       </div>
     </div>
   );
