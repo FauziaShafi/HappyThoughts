544ca59633783d50e617a1cc41af8102e5b17f2a
isiah Olvera <itolvera98@gmail.com>
2022-03-14 15:18:30 -0700

    short paragraph aboutus

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,7 +8,13 @@ export default function About() {
       <h1>About Us</h1>
       <hr />
       <p>
-        Here at Happy Programmers...
+        We are The Happy Four we are a new group of developers that are making a blog about programmers.
+        We wanted to bring everyone together to share their thoughts
+        so we created this app not only for us but for the rest of the world.
+        We wanna bring the whole community to participate in posting and commenting about how much they like programming. 
+        If you wanna wanna share your thoughts or meet new programmers here is the place to do it.
+        This app was created for us programmers.
+        and we want you to share with us.
       </p>
     </div>
   );
