a7fda0a9b99c6cbd5dbbf1fd9dac8008a9d945fe
gio_barranco <sandiego_94@hotmail.com>
2022-03-13 23:32:59 -0600

    frame for post page

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,6 +5,17 @@ export default function Post() {
         {/* TODO: display logged in user's username */}
         <h1>Latest Posts</h1>
         <hr />
+        <div className="post-title">
+          <p>Post Title</p>
+        </div>
+        <div className="enter-post">
+          <p>Post your Frustrations</p>
+        </div>
+        <div className="comment-section">
+          <p>Comments</p>
+          <div className="commenter-name">
+          </div>
+        </div>
         <p>
           ...
         </p>
