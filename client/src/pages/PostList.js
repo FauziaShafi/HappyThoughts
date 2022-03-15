412841c4a7c8bce4ea443be648eccabff685627d
Fauzia Shafi <fauziashafi12@gmail.com>
2022-03-15 10:31:33 -0700

    Client: Added functionality for PostList Page

--- client/src/pages/PostList.js
+++ client/src/pages/PostList.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
+import { QUERY_POSTS } from "../util/queries";
 
 export default function PostList() {
-  const postData = ["Happy thought title", "Happy day", "Whatever", "Again!!!"];
+  const posts = ["Happy thought title", "Happy day", "Whatever", "Again!!!"];
   const styles = {
     body: {
       width: "100%",
@@ -18,25 +19,33 @@ export default function PostList() {
       backgroundColor: "grey",
       padding: "10px",
     },
-    linkStyle: {
-      textDecoration: "none",
-      fonSize: "2rem",
-      color: "white",
-    },
+    linkStyle : {
+      textDecoration: 'none',
+      fonSize : "2rem",
+      color : "white"
+    }
   };
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const postData = data?.posts || [];
+  console.log(postData);
 
   return (
+    
     <div className="mb-8">
+     
       <div style={styles.body}>
-        <h1>All Posts </h1>
+      <h1>All Posts </h1>
         {postData.map((item) => (
           <li className="list-group-item" key={item.id} style={styles.link}>
-            <Link to={`/posts/ID`} style={styles.linkStyle}>
-              Title
-            </Link>
+            <Link to={`/posts/${item._id}`} style={styles.linkStyle}>{item.postTitle}</Link>
           </li>
         ))}
       </div>
+      <hr />
+    
+
+
+
     </div>
   );
 }
